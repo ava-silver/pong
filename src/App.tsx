2b30e213a00c40285cb137d58e7379854875e4c8
Ava Silver <avapsilver@gmail.com>
2022-11-08 22:29:09 -0500

    added game scoring logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { KeyboardEvent, useEffect, useRef, useState } from 'react';
+import React, { KeyboardEvent, useEffect, useState } from 'react';
 import Ball from './Ball';
 import Paddle from './Paddle';
 
@@ -8,7 +8,7 @@ export interface Position {
 }
 
 const UPDATE_TICK_MS = 20;
-const BALL_INITIAL_SPEED = 3;
+const BALL_INITIAL_SPEED = 15;
 type Velocity = Position;
 const PLAYER_SPEED = 10;
 
@@ -29,12 +29,15 @@ const playerMotion = {
   },
 };
 
-const MIN_X = 0;
-const MAX_X = 1200;
-const MIN_Y = 0;
-const MAX_Y = 800;
+const MIN_X = 40;
+const MAX_X = 1240;
+const MIN_Y = 8;
+const MAX_Y = 808;
 const BALL_SIZE = 48;
 const PLAYER_SIZE = 64;
+const GOAL_SIZE = 160;
+const BOARD_CENTER_Y = (MAX_Y + MIN_Y) / 2;
+const BOARD_CENTER_X = (MAX_X + MIN_X) / 2;
 
 function center(player: Position, size: number): Position {
   return { x: player.x + size / 2, y: player.y + size / 2 };
@@ -78,6 +81,14 @@ function reflect(player: Position, ball: Position, bV: Velocity): Velocity {
   }
 }
 
+export function goalScored(ball: Position): boolean {
+  return (
+    (ball.x <= MIN_X || ball.x + BALL_SIZE >= MAX_X - 5) &&
+    ball.y > BOARD_CENTER_Y - GOAL_SIZE / 2 &&
+    ball.y < BOARD_CENTER_Y + GOAL_SIZE / 2
+  );
+}
+
 function useBallPhysics(
   ball: Position,
   player: Position,
@@ -116,7 +127,7 @@ function App() {
   const [playerPos, setPlayerPos] = useState<Position>({ x: 1150, y: 50 });
   const [ballPos, setBallPos] = useState<Position>({ x: 500, y: 500 });
   const [ballVel, setBallVel] = useState<Velocity>({
-    x: BALL_INITIAL_SPEED,
+    x: 0,
     y: BALL_INITIAL_SPEED,
   });
   useEffect(() => {
@@ -132,6 +143,11 @@ function App() {
   }, [ballVel]);
 
   useBallPhysics(ballPos, playerPos, setBallVel);
+  useEffect(() => {
+    if (goalScored(ballPos)) {
+      console.log('scored');
+    }
+  }, [ballPos]);
 
   function handleInput(evt: KeyboardEvent) {
     // stops the player if theyre colliding with a ball to reduce jank physics
@@ -146,16 +162,34 @@ function App() {
   return (
     <div
       style={{
-        position: 'absolute',
-        left: '60px',
-        backgroundColor: 'black',
-        height: '50rem',
-        width: '75rem',
-      }}
-      tabIndex={0}
-      onKeyDown={handleInput}>
-      <Ball pos={ballPos} />
-      <Paddle pos={playerPos} />
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+      <div
+        style={{
+          backgroundColor: 'blue',
+          height: '10rem',
+          width: '2rem',
+        }}
+      />
+      <div
+        style={{
+          backgroundColor: 'black',
+          height: '50rem',
+          width: '75rem',
+        }}
+        tabIndex={0}
+        onKeyDown={handleInput}>
+        <Ball pos={ballPos} />
+        <Paddle pos={playerPos} />
+      </div>
+      <div
+        style={{
+          backgroundColor: 'blue',
+          height: '10rem',
+          width: '2rem',
+        }}
+      />
     </div>
   );
 }
